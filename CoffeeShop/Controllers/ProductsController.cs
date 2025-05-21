48520bb0487cf053db181ef52f11ce6e1ed8d32a
Sang0 <Sang0@ASUS_VIVOBOOK>
2025-05-21 18:15:32 +0700

    Add project files.

---
+++ CoffeeShop/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using CoffeeShop.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShop.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IProductRepository productRepository;
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+        public IActionResult Shop()
+        {
+            return View(productRepository.GetAllProducts());
+        }
+    }
+}
---
+++ CoffeeShop/Data/CoffeeshopDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CoffeeShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeShop.Data
+{
+    public class CoffeeshopDbContext : DbContext
+    {
+        public CoffeeshopDbContext(DbContextOptions<CoffeeshopDbContext>options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+
+        protected CoffeeshopDbContext()
+        {
+        }
+    }
+}
